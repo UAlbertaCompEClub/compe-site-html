eb72a4d5ad08ad9be87d7fe3153e1d2bd4d3ef2b
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-09-11 22:20:58 -0600

    Home skeleton

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,35 @@
 import React, { ReactElement } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-const Home: React.FC = (): ReactElement => (
-  <div>Compe logo + title + learnmore button</div>
-);
+import CompElogo from "./../img/logo-white-small.png";
+import BackgroundImage from "./../img/blurred-hardware.jpg";
 
+const useStyles = makeStyles(theme => ({
+  page: {
+    backgroundImage: `url(${BackgroundImage})`
+  },
+  button: {
+    margin: theme.spacing(1),
+    textTransform: "none"
+  },
+  input: {
+    display: "none"
+  }
+}));
+
+const Home: React.FC = (): ReactElement => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.page}>
+      <img src={CompElogo} alt="CompE logo" />
+      <h1>Computer Engineering Club</h1>
+      <span>Hardware, Software, and everything in between!</span>
+      <Button variant="outlined" className={classes.button}>
+        Learn More
+      </Button>
+    </div>
+  );
+};
 export default Home;
